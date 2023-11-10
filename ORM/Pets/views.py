80d8e55c470a3e063187fb59d720b8e489f9cd56
Sivaremo <sivaraj.t2001@gmail.com>
2023-11-10 09:20:41 +0530

    pets CRUD Doned

--- ORM/Pets/views.py
+++ ORM/Pets/views.py
@@ -13,3 +13,29 @@ class Pets_API(APIView):
             serializers.save()
             return Response({'message':'Created SucessFully'},status=status.HTTP_201_CREATED)
         return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def get(self,request):
+        id=request.query_parmas.get('id')
+        if id:
+            queryset=Pets.objects.get(id=id)
+            serializers=Pets_serializers(queryset)
+            return Response(serializers.data,status=status.HTTP_200_OK)
+        else:
+            queryset=Pets.objects.all()
+            serializers=Pets_serializers(queryset,many=True)
+            return Response(serializers.data,status=status.HTTP_200_OK)
+    def put(self,request):
+        id=request.query_parmas.get('id')
+        queryset=Pets.objects.get(id=id)
+        serializers=Pets_serializers(queryset,data=request.data) 
+        if serializers.is_valid():
+            serializers.save()
+            return Response({'message':'Updated Sucessfullyy'},status=status.HTTP_200_OK)
+        return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def delete(self,request):
+        id=request.query_parmas.get('id')
+        queryset=Pets.objects.get(id=id)
+        queryset.delete()
+        return Response({'message':'Deleted Sucessfully'},status=status.HTTP_200_OK)
+
+
