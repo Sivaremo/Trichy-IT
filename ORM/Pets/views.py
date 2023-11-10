abb009bbe40e4342eccdbd32e1ff9f610878c939
Sivaremo <sivaraj.t2001@gmail.com>
2023-11-10 10:01:32 +0530

    CRUD serizller error in postman

--- ORM/Pets/views.py
+++ ORM/Pets/views.py
@@ -12,16 +12,16 @@ class Pets_API(APIView):
         if serializers.is_valid():
             serializers.save()
             return Response({'message':'Created SucessFully'},status=status.HTTP_201_CREATED)
-        return Response(serializers.errors,status=status.HTTP_404_NOT_FOUND)
+        return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def get(self,request):
         id=request.query_params.get('id')
         if id:
             queryset=Pets.objects.get(id=id)
-            serializers=Pets_serializers(queryset)
+            serializers=Data_serializers(queryset)
             return Response(serializers.data,status=status.HTTP_200_OK)
         else:
             queryset=Pets.objects.all()
-            serializers=Pets_serializers(queryset,many=True)
+            serializers=Data_serializers(queryset,many=True)
             return Response(serializers.data,status=status.HTTP_200_OK)
     def put(self,request):
         id=request.query_params.get('id')
@@ -30,7 +30,7 @@ class Pets_API(APIView):
         if serializers.is_valid():
             serializers.save()
             return Response({'message':'Updated Sucessfullyy'},status=status.HTTP_200_OK)
-        return Response(serializers.errors,status=status.HTTP_404_NOT_FOUND)
+        return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def delete(self,request):
         id=request.query_params.get('id')
