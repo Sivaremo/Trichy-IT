19e34f414550246889641b056ebee0f4205b1a1d
Sivaremo <91781920+Sivaremo@users.noreply.github.com>
2023-11-30 19:50:51 +0530

    Delete ORM directory

--- ORM/ORM/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('petsdata/',include('Pets.urls')),
-    path('users/',include('Users.urls'))
-]
--- ORM/Users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-  path('reg/',Register_API.as_view()),
-  path('log/',Login_APi.as_view())
-]
